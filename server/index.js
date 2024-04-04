dfa616aefe1f3a9f3c06c02ee1cf6206251f2ff3
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-04-04 09:18:01 -0400

    debugging for deployment

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const path = require("path");
 const app = express();
- const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY;
 let fetch;
-import('node-fetch').then(nodeFetch => {
+import("node-fetch").then((nodeFetch) => {
   fetch = nodeFetch.default;
 });
 
@@ -15,22 +15,20 @@ const logRoutes = (req, res, next) => {
 };
 
 app.use(logRoutes);
-const distPath = path.join(__dirname, "../giphy-search/dist");
+const distPath = path.join(__dirname, "/giphy-search/dist");
 console.log(distPath);
 app.use(express.static(distPath));
 
 app.get("/api/gifs", async (req, res) => {
-   
-    console.log("apikey:", apiKey)
+  console.log("apikey:", apiKey);
   const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${3}`;
   const response = await fetch(url);
-    const json = await response.json();
-    console.log(json)
+  const json = await response.json();
+  console.log(json);
   res.json(json);
 });
 
 app.get("/api/gifs/search", async (req, res) => {
-
   const search = req.query.search;
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}`;
   const response = await fetch(url);
@@ -38,8 +36,6 @@ app.get("/api/gifs/search", async (req, res) => {
   res.json(json);
 });
 
-
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
-
